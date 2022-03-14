e75e7e1583f89df7fb1421305268fa7489bfebd3
emil.zamaldinov <emil.zamaldinov@lamoda.ru>
2022-03-14 22:27:16 +0300

    added lemmatization

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,33 @@
+import com.github.demidko.aot.PartOfSpeech
+import com.github.demidko.aot.WordformMeaning
 import java.io.File
 import java.net.URL
 import java.util.*
 
 fun main() {
-    Crawler().crawl(120)
+    Lemmatizer().start()
 }
+
+const val resultDirectoryPath = "./result"
+val resultDirectory = File(resultDirectoryPath)
+
+const val wordsFilePath = "./words.txt"
+val wordsFile = File(wordsFilePath)
+
+const val lemmasFilePath = "./lemmas.txt"
+val lemmasFile = File(lemmasFilePath)
 
 class Crawler {
 
     private val indexFile: File = File("./index.txt")
-    private val outputDirPath = "./result"
-    private val outputDirectory = File(outputDirPath)
     private var counter = 0
 
     init {
-        if (indexFile.exists()) {
-            indexFile.delete()
+        checkFileAndClean(indexFile)
+        if (resultDirectory.exists()) {
+            resultDirectory.deleteRecursively()
         }
-        indexFile.createNewFile()
-        if (outputDirectory.exists()) {
-            outputDirectory.deleteRecursively()
-        }
-        outputDirectory.mkdir()
+        resultDirectory.mkdir()
         counter = 0
     }
 
@@ -50,7 +56,7 @@ class Crawler {
     }
 
     private fun saveContentIntoFile(url: String, content: String): File {
-        val fileName = "$outputDirPath/$counter.txt"
+        val fileName = "$resultDirectoryPath/$counter.txt"
         val file = File(fileName)
         file.createNewFile()
         file.writeText(content)
@@ -60,4 +66,67 @@ class Crawler {
         return file
     }
 
+}
+
+class Lemmatizer {
+
+    private val wordsSet = mutableSetOf<String>()
+    private val lemmasSet = mutableSetOf<Pair<String, String>>()
+
+    private val unusedPartOfSpeech = listOf(
+        PartOfSpeech.Union,
+        PartOfSpeech.Numeral,
+        PartOfSpeech.OrdinalNumber,
+        PartOfSpeech.Pretext,
+        PartOfSpeech.Particle,
+    )
+
+    init {
+        checkFileAndClean(wordsFile)
+        checkFileAndClean(lemmasFile)
+    }
+
+    fun start() {
+        for (file in resultDirectory.listFiles() ?: emptyArray()) {
+            analyseFile(file)
+        }
+        writeWordsToFiles()
+    }
+
+    private fun analyseFile(file: File) {
+        val scanner = Scanner(file)
+        while (scanner.hasNext()) {
+            val word = scanner.next().lowercase()
+            if (!word.matches(Regex("[\"(]*[a-zA-Zа-яА-Я]+[;,!?)\"]*"))) {
+                continue
+            }
+            word.replace(Regex("[;,!?()\"]"), "")
+            val meanings = WordformMeaning.lookupForMeanings(word)
+            val lemma = meanings.getOrNull(0)?.lemma
+            if (unusedPartOfSpeech.contains(lemma?.partOfSpeech)) {
+                continue
+            }
+            lemma?.let {
+                wordsSet.add(word)
+                lemmasSet.add(Pair(it.toString(), word))
+            }
+        }
+    }
+
+    private fun writeWordsToFiles() {
+        for (word in wordsSet) {
+            wordsFile.appendText(word)
+            wordsFile.appendText("\n")
+        }
+        for (lemma in lemmasSet) {
+            lemmasFile.appendText("${lemma.first} ${lemma.second}\n")
+        }
+    }
+}
+
+fun checkFileAndClean(file: File) {
+    if (file.exists()) {
+        file.delete()
+    }
+    file.createNewFile()
 }
